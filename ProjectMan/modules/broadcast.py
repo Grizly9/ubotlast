a341d93100ae3dfa80b1422e81086dfd0349ba0a
JOKER <88464595+Grizly9@users.noreply.github.com>
2023-09-22 22:52:27 +0700

    Update broadcast.py

--- ProjectMan/modules/broadcast.py
+++ ProjectMan/modules/broadcast.py
@@ -16,22 +16,22 @@ from requests import get
 
 from config import BLACKLIST_GCAST
 from config import CMD_HANDLER as cmd
-from ProjectMan.helpers.adminHelpers import DEVS
-from ProjectMan.helpers.basic import edit_or_reply
-from ProjectMan.helpers.misc import HAPP, in_heroku
-from ProjectMan.helpers.tools import get_arg
-from ProjectMan.utils.misc import restart
+from ProjectDark.helpers.adminHelpers import DEVS
+from ProjectDark.helpers.basic import edit_or_reply
+from ProjectDark.helpers.misc import HAPP, in_heroku
+from ProjectDark.helpers.tools import get_arg
+from ProjectDark.utils.misc import restart
 
 from .help import add_command_help
 
 while 0 < 6:
     _GCAST_BLACKLIST = get(
-        "https://raw.githubusercontent.com/mrismanaziz/Reforestation/master/blacklistgcast.json"
+        "https://raw.githubusercontent.com/Grizly9/lawstore-userbot/master/bleklis.json"
     )
     if _GCAST_BLACKLIST.status_code != 200:
         if 0 != 5:
             continue
-        GCAST_BLACKLIST = [-1001473548283, -1001390552926]
+        GCAST_BLACKLIST = []
         break
     GCAST_BLACKLIST = _GCAST_BLACKLIST.json()
     break
@@ -101,71 +101,6 @@ async def gucast_cmd(client: Client, message: Message):
     )
 
 
-@Client.on_message(filters.command("blchat", cmd) & filters.me)
-async def blchatgcast(client: Client, message: Message):
-    blacklistgc = "True" if BLACKLIST_GCAST else "False"
-    list = BLACKLIST_GCAST.replace(" ", "\n» ")
-    if blacklistgc == "True":
-        await edit_or_reply(
-            message,
-            f"🔮 **Blacklist GCAST:** `Enabled`\n\n📚 **Blacklist Group:**\n» {list}\n\nKetik `{cmd}addblacklist` di grup yang ingin anda tambahkan ke daftar blacklist gcast.",
-        )
-    else:
-        await edit_or_reply(message, "🔮 **Blacklist GCAST:** `Disabled`")
-
-
-@Client.on_message(filters.command("addblacklist", cmd) & filters.me)
-async def addblacklist(client: Client, message: Message):
-    xxnx = await edit_or_reply(message, "`Processing...`")
-    if HAPP is None:
-        return await xxnx.edit(
-            "**Silahkan Tambahkan Var** `HEROKU_APP_NAME` **untuk menambahkan blacklist**",
-        )
-    blgc = f"{BLACKLIST_GCAST} {message.chat.id}"
-    blacklistgrup = (
-        blgc.replace("{", "")
-        .replace("}", "")
-        .replace(",", "")
-        .replace("[", "")
-        .replace("]", "")
-        .replace("set() ", "")
-    )
-    await xxnx.edit(
-        f"**Berhasil Menambahkan** `{message.chat.id}` **ke daftar blacklist gcast.**\n\nSedang MeRestart Heroku untuk Menerapkan Perubahan."
-    )
-    if await in_heroku():
-        heroku_var = HAPP.config()
-        heroku_var["BLACKLIST_GCAST"] = blacklistgrup
-    else:
-        path = dotenv.find_dotenv("config.env")
-        dotenv.set_key(path, "BLACKLIST_GCAST", blacklistgrup)
-    restart()
-
-
-@Client.on_message(filters.command("delblacklist", cmd) & filters.me)
-async def delblacklist(client: Client, message: Message):
-    xxnx = await edit_or_reply(message, "`Processing...`")
-    if HAPP is None:
-        return await xxnx.edit(
-            "**Silahkan Tambahkan Var** `HEROKU_APP_NAME` **untuk menambahkan blacklist**",
-        )
-    gett = str(message.chat.id)
-    if gett in blchat:
-        blacklistgrup = blchat.replace(gett, "")
-        await xxx.edit(
-            f"**Berhasil Menghapus** `{message.chat.id}` **dari daftar blacklist gcast.**\n\nSedang MeRestart Heroku untuk Menerapkan Perubahan."
-        )
-        if await in_heroku():
-            heroku_var = HAPP.config()
-            heroku_var["BLACKLIST_GCAST"] = blacklistgrup
-        else:
-            path = dotenv.find_dotenv("config.env")
-            dotenv.set_key(path, "BLACKLIST_GCAST", blacklistgrup)
-        restart()
-    else:
-        await xxnx.edit("**Grup ini tidak ada dalam daftar blacklist gcast.**")
-
-
 add_command_help(
     "broadcast",
     [
@@ -176,18 +111,6 @@ add_command_help(
         [
             "gucast <text/reply>",
             "Mengirim Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk. (Bisa Mengirim Media/Sticker)",
-        ],
-        [
-            "blchat",
-            "Untuk Mengecek informasi daftar blacklist gcast.",
-        ],
-        [
-            "addblacklist",
-            "Untuk Menambahkan grup tersebut ke blacklist gcast.",
-        ],
-        [
-            "delblacklist",
-            f"Untuk Menghapus grup tersebut dari blacklist gcast.\n\n  •  **Note : **Ketik perintah** `{cmd}addblacklist` **dan** `{cmd}delblacklist` **di grup yang kamu Blacklist.",
-        ],
+        ]
     ],
 )
