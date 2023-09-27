a1db50f98e5716b5b1e30ee50777d04c893c6a8c
JOKER <88464595+Grizly9@users.noreply.github.com>
2023-09-28 05:57:24 +0700

    Update heroku.py

--- ProjectMan/modules/heroku.py
+++ ProjectMan/modules/heroku.py
@@ -32,98 +32,6 @@ from .help import add_command_help
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-@Client.on_message(filters.command("setvar", CMD_HANDLER) & filters.me)
-async def set_var(client: Client, message: Message):
-    if len(message.command) < 3:
-        return await edit_or_reply(
-            message, f"<b>Usage:</b> {CMD_HANDLER}setvar [Var Name] [Var Value]"
-        )
-    Man = await edit_or_reply(message, "`Processing...`")
-    to_set = message.text.split(None, 2)[1].strip()
-    value = message.text.split(None, 2)[2].strip()
-    if await in_heroku():
-        if HAPP is None:
-            return await Man.edit(
-                "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
-            )
-        heroku_config = HAPP.config()
-        if to_set in heroku_config:
-            await Man.edit(f"Berhasil Mengubah var {to_set} menjadi {value}")
-        else:
-            await Man.edit(f"Berhasil Menambahkan var {to_set} menjadi {value}")
-        heroku_config[to_set] = value
-    else:
-        path = dotenv.find_dotenv("config.env")
-        if not path:
-            return await Man.edit(".env file not found.")
-        dotenv.set_key(path, to_set, value)
-        if dotenv.get_key(path, to_set):
-            await Man.edit(f"Berhasil Mengubah var {to_set} menjadi {value}")
-        else:
-            await Man.edit(f"Berhasil Menambahkan var {to_set} menjadi {value}")
-        restart()
-
-
-@Client.on_message(filters.command("getvar", CMD_HANDLER) & filters.me)
-async def varget_(client: Client, message: Message):
-    if len(message.command) != 2:
-        return await edit_or_reply(
-            message, f"<b>Usage:</b> {CMD_HANDLER}getvar [Var Name]"
-        )
-    Man = await edit_or_reply(message, "`Processing...`")
-    check_var = message.text.split(None, 2)[1]
-    if await in_heroku():
-        if HAPP is None:
-            return await Man.edit(
-                "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
-            )
-        heroku_config = HAPP.config()
-        if check_var in heroku_config:
-            return await Man.edit(
-                f"<b>{check_var}:</b> <code>{heroku_config[check_var]}</code>"
-            )
-        else:
-            return await Man.edit(f"Tidak dapat menemukan var {check_var}")
-    else:
-        path = dotenv.find_dotenv("config.env")
-        if not path:
-            return await Man.edit(".env file not found.")
-        output = dotenv.get_key(path, check_var)
-        if not output:
-            await Man.edit(f"Tidak dapat menemukan var {check_var}")
-        else:
-            return await Man.edit(f"<b>{check_var}:</b> <code>{str(output)}</code>")
-
-
-@Client.on_message(filters.command("delvar", CMD_HANDLER) & filters.me)
-async def vardel_(client: Client, message: Message):
-    if len(message.command) != 2:
-        return await message.edit(f"<b>Usage:</b> {CMD_HANDLER}delvar [Var Name]")
-    Man = await edit_or_reply(message, "`Processing...`")
-    check_var = message.text.split(None, 2)[1]
-    if await in_heroku():
-        if HAPP is None:
-            return await Man.edit(
-                "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
-            )
-        heroku_config = HAPP.config()
-        if check_var in heroku_config:
-            await message.edit(f"Berhasil Menghapus var {check_var}")
-            del heroku_config[check_var]
-        else:
-            return await message.edit(f"Tidak dapat menemukan var {check_var}")
-    else:
-        path = dotenv.find_dotenv("config.env")
-        if not path:
-            return await message.edit(".env file not found.")
-        output = dotenv.unset_key(path, check_var)
-        if not output[0]:
-            return await message.edit(f"Tidak dapat menemukan var {check_var}")
-        else:
-            await message.edit(f"Berhasil Menghapus var {check_var}")
-        restart()
-
-
 @Client.on_message(filters.command("usage", CMD_HANDLER) & filters.me)
 async def usage_heroku(client: Client, message: Message):
     ### Credits CatUserbot
@@ -204,9 +112,6 @@ async def usange_heroku(client: Client, message: Message):
 add_command_help(
     "heroku",
     [
-        ["setvar", "Untuk mengatur variabel config userbot."],
-        ["delvar", "Untuk menghapus variabel config userbot."],
-        ["getvar", "Untuk melihat variabel config userbot."],
         [
             f"usage atau {CMD_HANDLER}dyno",
             "Untuk mengecheck kouta dyno heroku.",
